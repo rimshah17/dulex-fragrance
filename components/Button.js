04b65829cc8b26a23691e10bd740a5edacca9dd0
rimshah17 <rimshahali11@gmail.com>
2025-01-09 09:49:39 +0500

    Remove all files from the repository

--- components/Button.js
+++
@@ -1,7 +0,0 @@
-export default function Button({ text }) {
-  return (
-    <button className="bg-blue-500 btn px-6 py-3 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
-      {text}
-    </button>
-  );
-}
